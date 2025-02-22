665684b05f9c8067ca513642d91dff599ea26de7
borisdvlpr <eduffmiranda@hotmail.com>
2025-02-23 00:20:45 +0100

    feat: add "persistency" to traffic control with a file

--- src/main/kotlin/TrafficControl.kt
+++ src/main/kotlin/TrafficControl.kt
@@ -5,25 +5,43 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
 val blockedEndpoints = mutableSetOf<String>()
+const val BLOCKED_PATHS_FILE = "blocked_endpoints.txt"
 
 fun Application.configureTrafficControl() {
+    loadBlockedEndpoints()
+
     routing {
-        post("proxy/block") {
+        post("/proxy/block") {
             val endpoint = call.receive<String>()
             blockedEndpoints.add(endpoint)
+            saveBlockedEndpoints()
             call.respond(HttpStatusCode.OK, "Blocked $endpoint")
         }
 
-        post("proxy/unblock") {
+        post("/proxy/unblock") {
             val endpoint = call.receive<String>()
             blockedEndpoints.remove(endpoint)
-            call.respond(HttpStatusCode.OK, "Blocked $endpoint")
+            saveBlockedEndpoints()
+            call.respond(HttpStatusCode.OK, "Unblocked $endpoint")
         }
 
         get("/proxy/blocked") {
             call.respond(HttpStatusCode.OK, blockedEndpoints.joinToString(", "))
         }
     }
+}
+
+fun loadBlockedEndpoints() {
+    val file = File(BLOCKED_PATHS_FILE)
+    if (file.exists()) {
+        blockedEndpoints.clear()
+        blockedEndpoints.addAll(file.readLines().filter { it.isNotBlank() })
+    }
+}
+
+fun saveBlockedEndpoints() {
+    File(BLOCKED_PATHS_FILE).writeText(blockedEndpoints.joinToString("\n"))
 }
